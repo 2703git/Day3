d10e33f6a4269ffbb173d0600b008987104b6a38
for_mee <250182@newuu.uz>
2026-08-06 22:20:59 +0500

    Add files via upload

---
+++ Pr9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pr9 {
+    public static int differ(int n){
+        if (n>0){
+            return n*15;
+        } else if (n<0) {
+            return Math.abs(n);
+        }else {
+            return n;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = scanner.nextInt();
+        System.out.println("Result: "+differ(n));
+    }
+}
